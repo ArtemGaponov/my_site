56b5c277fb4697f9caa25c24768f20c5fa0f8693
SuhoviyEugene <suhoviy.eugene@gmail.com>
2013-11-15 21:50:42 +0200

    first

---
+++ sesion.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+
+mysql_connect('localhost', 'root', '201288') or die ('нихуя не подключилось');
+mysql_select_db('artemka');
+$q = mysql_query('select * from q');
+while ($res = mysql_fetch_assoc($q)) {
+    var_dump($res);
+}
+
+
+
+
+
+
